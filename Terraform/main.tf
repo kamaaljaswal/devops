34163e30dad4f7813421ccaddf1c0f06f1199cce
Osama Sajed <46377060+kamaaljaswal@users.noreply.github.com>
2024-02-13 11:59:47 +0500

    Feature/terraform new deployment instance (#2)
    * Add admin security group
    
    * Add deployment instance
    
    * Update admin sg outbound config

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,3 +20,25 @@ resource "aws_instance" "staging" {
     ignore_changes = [ ami ]
   }
 }
+
+resource "aws_instance" "jenkins" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = data.aws_key_pair.osama.key_name
+
+  root_block_device {
+    volume_size = "32"
+    volume_type = "gp3"
+    encrypted = true
+  }
+
+  vpc_security_group_ids = [ aws_security_group.admin.id ]
+
+  tags = {
+    "Name": "Jenkins"
+  }
+
+  lifecycle {
+    ignore_changes = [ ami ]
+  }
+}
